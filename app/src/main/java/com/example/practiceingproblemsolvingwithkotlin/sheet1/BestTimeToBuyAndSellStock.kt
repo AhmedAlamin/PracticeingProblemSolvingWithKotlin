2a9e3fff18cde4d7b169bfdf00f98544ad442588
Ahmed Mohamed <engahmedegypto@gmail.com>
2023-06-23 20:21:40 +0300

    updated Solution BestTimeToBuyAndSellStock.kt

--- app/src/main/java/com/example/practiceingproblemsolvingwithkotlin/sheet1/BestTimeToBuyAndSellStock.kt
+++ app/src/main/java/com/example/practiceingproblemsolvingwithkotlin/sheet1/BestTimeToBuyAndSellStock.kt
@@ -6,19 +6,20 @@ class Solution {
     fun maxProfit(prices: IntArray): Int {
 
 
-
         var maxProfit = Int.MIN_VALUE
         var maxNumber = prices[prices.size-1]
 
-        for (i in (prices.size-1) downTo  0){
+        for (i in (prices.size-1) downTo 0){
             maxNumber = max(maxNumber,prices[i])
-               if (maxNumber - prices[i] > maxProfit){
-                   maxProfit = maxNumber - prices[i]
-               }
+            if (maxNumber - prices[i] > maxProfit){
+                maxProfit = maxNumber - prices[i]
+            }
 
         }
-        return if (maxProfit == Int.MIN_VALUE) 0
+
+        return if (maxProfit.equals(Int.MIN_VALUE)) 0
         else maxProfit
+
 
     }
 }
